508c564e8c5b8747e2d65a7308db77c927fc32a3
0xfoobar <0xfoobar1@gmail.com>
2023-10-24 00:09:07 -0400

    remove doubled string-bytes32 conversion logic

--- src/MarketMetadata.sol
+++ src/MarketMetadata.sol
@@ -147,26 +147,4 @@ contract MarketMetadata is Ownable2Step, ERC2981 {
             mstore(0x40, add(result, 0x40))
         }
     }
-
-    function _bytes32ToString(bytes32 _bytes32) internal pure returns (string memory) {
-        uint8 i = 0;
-        bytes memory bytesArray = new bytes(64);
-        for (i = 0; i < bytesArray.length; i++) {
-            uint8 _f = uint8(_bytes32[i / 2] & 0x0f);
-            uint8 _l = uint8(_bytes32[i / 2] >> 4);
-
-            bytesArray[i] = _toByte(_f);
-            i = i + 1;
-            bytesArray[i] = _toByte(_l);
-        }
-        return string(bytesArray);
-    }
-
-    function _toByte(uint8 _uint8) internal pure returns (bytes1) {
-        if (_uint8 < 10) {
-            return bytes1(_uint8 + 48);
-        } else {
-            return bytes1(_uint8 + 87);
-        }
-    }
 }
